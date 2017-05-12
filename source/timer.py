6128a6cdb3de5b9add175656deba380ce1fcfa0f
XJiang <Xin.Jiang@harman.com>
2017-05-12 18:11:31 +0800

    modify timer

--- source/timer.py
+++ source/timer.py
@@ -14,7 +14,12 @@ def fun_timer():
 #time.sleep(200)
 #timer.cancel()
 #weather.weather_func('2017-05-11 12:00:00')
+schedule.every().day.at("00:00").do(fun_timer)
+schedule.every().day.at("04:00").do(fun_timer)
+schedule.every().day.at("08:00").do(fun_timer)
+schedule.every().day.at("12:00").do(fun_timer)
 schedule.every().day.at("16:00").do(fun_timer)
+schedule.every().day.at("20:00").do(fun_timer)
 while True:
     schedule.run_pending()
     time.sleep(1)
--- source/timer_weather.py
+++ source/timer_weather.py
@@ -9,18 +9,10 @@ sys.setdefaultencoding('utf8');
 def fun_timer():
     print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
     weather.weather_func( time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))  )
-    #global timer
-    #timer = threading.Timer(10, fun_timer)
-    #timer.start()
-
-#timer = threading.Timer(0.02, fun_timer)
-#timer.start()
-
-#time.sleep(200)
-#timer.cancel()
-#weather.weather_func('2017-05-11 12:00:00')
-
-schedule.every().hour.do(fun_timer)
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+print time.localtime(time.time())
+weather.weather_func( '2017-05-12 17:00:00' )
+#schedule.every().hour.do(fun_timer)
+#schedule.every().day.at("17:00").do(fun_timer)
+#while True:
+#    schedule.run_pending()
+#    time.sleep(1)
